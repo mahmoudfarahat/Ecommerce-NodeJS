3bbc8badd92036712a1e31952345a85a8a63b25b
Mahmoud Farahat <mahmoud.ped@gmail.com>
2024-08-01 07:50:14 +0300

    1.2

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(cookieSession({
     keys:['dsadsadsadsadsa']
 }))
-app.get('/',(req,res)=>{
+app.get('/signup',(req,res)=>{
     res.send(`
             <div>
+           Your Id is ${req.session.userId}
                 <form method="POST">
                     <input name="email" placeholder="email" />
                     <input name="password" placeholder="password" />
@@ -37,7 +38,7 @@ app.get('/',(req,res)=>{
 //         next()
 //     }
 // }
-app.post('/', async (req,res)=>{
+app.post('/signup', async (req,res)=>{
     console.log(req.body)
     const {email , password , passwordConfirmation } = req.body
 
@@ -57,6 +58,40 @@ if(password !== passwordConfirmation ){
     res.send('Account created!!')
 })
 
+app.get('/signout', (req,res)=>{
+req.session = null;
+res.send('You Are logged out')
+})
+
+app.get('/signin',(req,res)=>{
+    res.send(`
+         <div>
+                 <form method="POST">
+                    <input name="email" placeholder="email" />
+                    <input name="password" placeholder="password" />
+                     <button>Sign In</button>
+                </form>
+            </div>
+        `)
+})
+
+
+app.post('/signin',async (req,res)=>{
+    const {email , password} = req.body;
+    const user = await usersRepo.getOneBy({email});
+    if(!user){
+        return res.send('Email not Found')
+    }
+
+    if(user.password !== password){
+        return res.send('Invalid password')
+    }
+    req.session.userId = user.id;
+    return res.send('You are signed in')
+
+})
+
+
 app.listen(3000,()=>{
-    console.log('http://localhost:3000')
+    console.log('http://localhost:3000/signup')
 })
